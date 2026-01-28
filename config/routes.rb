3db4327b71ee3ad4f8e90d8198037430b55d8d48
Ruxadev <grmdev01@gmail.com>
2026-01-28 16:28:29 -0300

    feat: Nest todo routes within location resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :locations
+      resources :locations do
+        resources :todos
+      end
     end
   end
 
