e1be9bbb5ae0816bf86968c6266577ee3d1b2b0f
Ruxadev <grmdev01@gmail.com>
2026-01-28 16:42:39 -0300

    feat(todos): Add before_action to set parent location

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::TodosController < ApplicationController
+  before_action :set_location
+
+  private
+
+  def set_location
+    @location = Location.find(params[:location_id])
+  end
 end
