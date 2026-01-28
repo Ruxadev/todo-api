6ee1fb41ac6e438cc0f28d9d90faa5e2b20b40c4
Ruxadev <grmdev01@gmail.com>
2026-01-28 14:54:25 -0300

    feat(locations): Implement create action with strong parameters

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::LocationsController < ApplicationController
     @locations = Location.all
     render json: @locations
   end
+
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      render json: @location, status: :created, location: api_v1_location_url(@location)
+    else
+      render json: @location.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name)
+  end
 end
